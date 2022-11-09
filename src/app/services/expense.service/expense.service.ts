511a52503267f140901bc4ab434ed0ff54a36b72
Mauricio Corredor <malec@uniandes.edu.co>
2022-11-08 21:52:20 -0500

    bug con sort()

--- src/app/services/expense.service/expense.service.ts
+++ src/app/services/expense.service/expense.service.ts
@@ -124,5 +124,13 @@ export class ExpenseService {
     );
   }
 
+newFunction(){
+
+  let myarray = [80, 3, 9, 34, 23, 5, 1];
+
+  myarray.sort();
+  console.log(myarray); // outputs: [1, 23, 3, 34, 5, 80, 9]
+}
+
 
 }
